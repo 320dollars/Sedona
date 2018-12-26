7c45043faa7d8e7b09d7c41f50ea85b43192ce38
320dollars <44992926+320dollars@users.noreply.github.com>
2018-12-26 20:59:48 +0300

    осталось доделать мелкие детали формы написать скрипт и отредактировать css

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@
 
 var openSearchBtn = document.querySelector('.btn');
 var modalWindowSearch = document.querySelector('.search');
+var inputForFocus = modalWindowSearch.querySelector('.begin');
 
 openSearchBtn.addEventListener('click', function(){
   modalWindowSearch.classList.toggle('search-show');
+  inputForFocus.focus();
 });
