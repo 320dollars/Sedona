cba5967f2d89ed1cd6cbb81684caa47218f33846
320dollars <44992926+320dollars@users.noreply.github.com>
2019-01-18 11:20:53 +0300

    небольшие исправления верстки и скиптов

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,10 @@ var plusButtonSecond = modalWindowSearch.querySelector('.second-plus');
 var minusButtonSecond = modalWindowSearch.querySelector('.second-minus');
 var numberChildren = modalWindowSearch.querySelector('.first');
 var form = modalWindowSearch.querySelector('.info-list');
+var oneRange = document.querySelector('#min-result');
+var oneOut = document.querySelector('#one-result');
+var checkIn = form.querySelector('#checkin');
+var checkOut = form.querySelector('#checkout');
 
 openSearchBtn.addEventListener('click', function () {
   modalWindowSearch.classList.toggle('search-show');
@@ -20,7 +24,6 @@ openSearchBtn.addEventListener('click', function () {
 });
 
 window.addEventListener('keydown', function (evt) {
-  evt.preventDefault();
   if (evt.keyCode === ESC_Button) {
     if (modalWindowSearch.classList.contains('search-show')) {
       modalWindowSearch.classList.remove('search-show');
@@ -47,6 +50,41 @@ var deleteClass = function() {
   numberChildren.classList.remove('no-valid');
 }
 
+var months = [
+  'Января',
+  'Февраля',
+  'Марта',
+  'Апреля',
+  'Мая',
+  'Июня',
+  'Июля',
+  'Августа',
+  'Сентября',
+  'Октября',
+  'Ноября',
+  'Декабря'
+]
+
+var today = new Date();
+var startDay = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+var tomorrow = new Date(startDay.valueOf() + 86400000);
+
+var getFormatDate = function (time) {
+  var dd = time.getDate();
+  var mm = time.getMonth();
+  var yyyy = time.getFullYear();
+
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+
+  time = dd + ' ' + months[mm]  + ' ' + yyyy;
+  return time;
+}
+
+checkIn.value = getFormatDate(today);
+checkOut.value = getFormatDate(tomorrow);
+
 form.addEventListener('submit', function (evt) {
   if (numberAdolts.value <= 0) {
     evt.preventDefault();
@@ -58,4 +96,55 @@ form.addEventListener('submit', function (evt) {
     numberChildren.classList.add('no-valid');
     setTimeout(deleteClass, 3500);
   }
+  if (checkIn.value.valueOf() < startDay.valueOf()) {
+    evt.preventDefault();
+    checkIn.classList.add('no-valid');
+    setTimeout(deleteClass, 3500);
+  }
 });
+
+
+
+
+
+
+/*var getDay = function (date) {
+  var day = date.getDay();
+  if (day == 0) day = 7;
+  return day - 1;
+}*/
+
+/*var createCalendar = function (id, year, month) {
+  var element = form.querySelector(id);
+  var mon = month - 1;
+  var d = new Date(year, mon);
+  var table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr>';
+  for (var i = 0; i < getDay(d); i++) {
+    table += '<td></td>';
+  }
+
+  while (d.getMonth() == mon) {
+    table += '<td>' + d.getDate() + '</td>';
+
+    if (getDay(d) % 7 == 6) {
+      table += '<tr></tr>';
+    }
+
+    d.setDate(d.setDate() + 1);
+  }
+
+  if (getDay(d) != 0 ) {
+      for (var i = getDay(d); i < 7; i++) {
+      table += '<td></td>';
+    }
+  }
+
+  table += '</tr></table>';
+  element.innerHTML = table;
+}
+
+createCalendar('#calendar', 2019, 1);*/
+
+/*oneRange.addEventListener('oninput', function() {
+  oneRange.value = oneOut.value;
+})*/
